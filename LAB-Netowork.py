d51871ab17fab1f608606e3626d35f0874064141
NAT SIRIRUANGBUN <nat.siriruangbun@gmail.com>
2025-08-28 12:30:06 +0700

    -_

--- LAB-Netowork.py
+++ LAB-Netowork.py
@@ -4,11 +4,9 @@ import time
 SERIAL_PORT = '/dev/serial0' # ตรวจสอบให้แน่ใจว่าถูกต้อง
 BAUD_RATE = 115200
 ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=2)
-url = 'https://iot-backend-iihizzkyh-nat-siriruangbuns-projects.vercel.app/data/Distance?amount='
 
 while True:
 
     line = ser.readline().decode('utf-8').rstrip()
-    print(line)
-    # strr = url + str(line)
-    # print(strr)
+    url = f'https://iot-backend-iihizzkyh-nat-siriruangbuns-projects.vercel.app/data/Distance?amount={line}'
+    print(url)
